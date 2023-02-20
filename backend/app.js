723de4e6a9a674dac6babc4ceaa3425bb0a1d756
Samuel Hovie <samhovie@Samuels-MacBook-Air.local>
2023-02-20 17:15:55 -0500

    error middleware

--- backend/app.js
+++ backend/app.js
@@ -41,6 +41,15 @@ csurf({
 })
 );
 
+// Catch unhandled requests and forward to error handler.
+app.use((_req, _res, next) => {
+    const err = new Error("The requested resource couldn't be found.");
+    err.title = "Resource Not Found";
+    err.errors = { message: "The requested resource couldn't be found." };
+    err.status = 404;
+    next(err);
+});
+
 app.use(routes);
 
 
