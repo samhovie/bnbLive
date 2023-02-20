694b6113f796027ec8092ae57117226add094c74
Samuel Hovie <samhovie@Samuels-MacBook-Air.local>
2023-02-20 17:28:19 -0500

    app.js update

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,7 @@ const csurf = require('csurf');
 const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
 const routes = require('./routes');
+const { ValidationError } = require('sequelize');
 
 const { environment } = require('./config');
 const isProduction = environment === 'production';
@@ -41,6 +42,9 @@ csurf({
 })
 );
 
+
+app.use(routes);
+
 // Catch unhandled requests and forward to error handler.
 app.use((_req, _res, next) => {
     const err = new Error("The requested resource couldn't be found.");
@@ -50,7 +54,36 @@ app.use((_req, _res, next) => {
     next(err);
 });
 
-app.use(routes);
+
+
+
+  // ...
+
+// Process sequelize errors
+app.use((err, _req, _res, next) => {
+// check if error is a Sequelize error:
+if (err instanceof ValidationError) {
+    let errors = {};
+    for (let error of err.errors) {
+    errors[error.path] = error.message;
+    }
+    err.title = 'Validation error';
+    err.errors = errors;
+}
+next(err);
+});
+
+// Error formatter
+app.use((err, _req, res, _next) => {
+    res.status(err.status || 500);
+    console.error(err);
+    res.json({
+      title: err.title || 'Server Error',
+      message: err.message,
+      errors: err.errors,
+      stack: isProduction ? null : err.stack
+    });
+});
 
 
 module.exports = app;
