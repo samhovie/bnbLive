1255d586063ffdefca5d4875e168ea3738661d79
Samuel Hovie <samhovie@Samuels-MacBook-Air.local>
2023-03-10 18:08:01 -0500

    null fix

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,12 +5,29 @@ import { Provider as ReduxProvider } from 'react-redux';
 import './index.css';
 import App from './App';
 import configureStore from './store';
+// frontend/src/index.js
+// ... other imports
+import { restoreCSRF, csrfFetch } from './store/csrf';
+import * as ReactDOMClient from 'react-dom/client';
 
 const store = configureStore();
+const root = ReactDOMClient.createRoot (document.getElementById ("root"));
+
+if (process.env.NODE_ENV !== 'production') {
+  restoreCSRF();
+
+  window.csrfFetch = csrfFetch;
+  window.store = store;
+}
+
+// const store = configureStore();
 
 if (process.env.NODE_ENV !== "production") {
   window.store = store;
 }
+
+console.log(window)
+
 
 function Root() {
   return (
@@ -22,9 +39,8 @@ function Root() {
   );
 }
 
-ReactDOM.render(
+root.render(
   <React.StrictMode>
     <Root />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
