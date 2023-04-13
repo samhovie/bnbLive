d4a5a6db37ded36e2127db24deb971d355404b77
Samuel Hovie <samhovie@umich.edu>
2023-04-13 11:44:48 -0400

    navbar layout

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
-import { useSelector } from 'react-redux';
-import ProfileButton from './ProfileButton';
+import React from "react";
+import { NavLink } from "react-router-dom";
+import { useSelector } from "react-redux";
+import ProfileButton from "./ProfileButton";
 // import OpenModalButton from '../OpenModalButton';
 // import LoginFormModal from '../LoginFormModal';
 // import SignupFormModal from '../SignupFormModal';
-import './Navigation.css';
+import "./Navigation.css";
 
-function Navigation({ isLoaded }){
-  const sessionUser = useSelector(state => state.session.user);
+function Navigation({ isLoaded }) {
+  const sessionUser = useSelector((state) => state.session.user);
 
   // let sessionLinks;
   // if (sessionUser) {
@@ -33,41 +33,35 @@ function Navigation({ isLoaded }){
   // }
 
   return (
+    <div className=" navigation-container flex-sb">
+      <div className="nav-item flex-ctr">
+        <NavLink to="/">
+          <div className="flex-ctr">
+            <img
+              style={{ width: "48px", height: "48px" }}
+              alt="logo"
+              src="https://livebnbbucket.s3.amazonaws.com/favicon.png"
+            ></img>
+          </div>
 
-
-    <div className=' navigation-container flex-sb'>
-
-      <div className='nav-item flex-ctr'>
-
-        <NavLink to='/'>
-        <div>
-          {/* <i className="fa-solid fa-microphone-lines"></i> */}
-          <img style={{width:'48px', height:'48px'}} className='flex-ctr' alt='logo' src="https://livebnbbucket.s3.amazonaws.com/favicon.png"></img>
-        </div>
-
-        <div className='flex-ctr'>
-          <h1>bnbLive</h1>
-        </div>
+          <div className="flex-ctr">
+            <h1>bnbLive</h1>
+          </div>
         </NavLink>
-
       </div>
 
-      <div  className='nav-item flex-ctr'>
-
-        <div>
-        <NavLink to={`/spots/new`}>Create a Spot</NavLink>
-
+      <div className="flex-sb nav-item ">
+        <div style={{padding:'0 35px'}}>
+          <NavLink to={`/spots/new`}>Create a Spot</NavLink>
         </div>
 
         {isLoaded && (
-      <div>
-      <ProfileButton user={sessionUser} />
-      </div>
+          <div className="flex-ctr">
+            <ProfileButton user={sessionUser} />
+          </div>
         )}
-
       </div>
     </div>
-
   );
 }
 
